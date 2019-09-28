68f8361a61520db1ab5cd5c2921d304009f40db1
dharada1 <haradadaichi715@gmail.com>
2019-09-28 23:01:59 +0900

    db

--- go/mysql/mysql-connections.go
+++ go/mysql/mysql-connections.go
@@ -1,5 +1,6 @@
 // https://gist.github.com/catatsuy/e627aaf118fbe001f2e7c665fda48146
 // http://dsas.blog.klab.org/archives/2018-02/configure-sql-db.html
+// https://www.alexedwards.net/blog/configuring-sqldb
 
 db, _ = sql.Open("mysql", dsn)
 
