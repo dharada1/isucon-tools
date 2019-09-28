ebf24676bf07d2c4e413a7672162b292be652dc2
dharada1 <haradadaichi715@gmail.com>
2019-09-28 22:21:53 +0900

    httpパッケージ系

---
+++ go/http/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Goでnet/httpを使う時のこまごまとした注意
+// https://qiita.com/ono_matope/items/60e96c01b43c64ed1d18
+
+func main() {
+	nethttpパッケージのパラメータチューニング()
+	レスポンスを受け取ったら必ずBodyをCloseする()
+}
+
+func nethttpパッケージのパラメータチューニング() {
+	// http.Clientはパッケージ側にグローバル変数として存在する(スレッドセーフ).
+	// const DefaultMaxIdleConnsPerHost = 2 がデフォルトなので、適切に変更してやる.
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 1000
+
+	// DefaultTransport, Timeout, MaxIdleConns, MaxIdleConnsPerHost, IdleConnTimeout
+	// など他にも
+
+	// 事例 (適切な値いくつだろ)
+	/*
+	   外部 API へのリクエストをする際に HTTP Agent の MaxIdleConns と MaxIdleConnsPerHost を拡張、1 ホスト 3000 コネクションまで持てるようにした。
+	   https://yosuke-furukawa.hatenablog.com/entry/2019/09/08/232231
+	*/
+
+	// 自分で用意したhttp.Clientの制限を変更する場合以下のようになるらしい(未確認)
+	// c := http.Client{
+	// 	Transport: http.Transport{MaxIdleConnsPerHost: 32},
+	// }
+	// _, _ = c.Get("http://example.com")
+}
+
+func レスポンスを受け取ったら必ずBodyをCloseする() {
+	// 呼び出し側でCloseしてあげないとTCPコネクションが再利用されないため.
+	// https://qiita.com/ono_matope/items/60e96c01b43c64ed1d18
+
+	f := func(url string) error {
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			return err
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close() // 明示的にクローズ
+
+		return nil
+	}
+
+	url := "https://pairs.lv/1.0/master"
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		f(url)
+	}
+}
