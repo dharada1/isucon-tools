8fedbb5ee38a4d11c7a6c5beeae5c461ee97c450
dharada1 <haradadaichi715@gmail.com>
2019-09-28 23:00:32 +0900

    readme

---
+++ go/mysql/mysql-connections.go
@@ -0,0 +1,14 @@
+// https://gist.github.com/catatsuy/e627aaf118fbe001f2e7c665fda48146
+// http://dsas.blog.klab.org/archives/2018-02/configure-sql-db.html
+
+db, _ = sql.Open("mysql", dsn)
+
+maxConns := os.Getenv("DB_MAXOPENCONNS")
+if maxConns != "" {
+	i, err := strconv.Atoi(maxConns)
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(i)
+	db.SetMaxIdleConns(i)
+}
